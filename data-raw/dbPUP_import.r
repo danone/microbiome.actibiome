c18418dd71d69f7f85abd020f6d451f5fc4daf83
Julien Tap <Julien.Tap@danone.com>
2022-05-26 03:06:13 +0200

    export bif nr95 genes hits on dbPUP

--- data-raw/dbPUP_import.r
+++ data-raw/dbPUP_import.r
@@ -10,3 +10,19 @@
 #   readr::write_tsv(dbPUP_clean, file="dbPUP_clean.tsv")
 
 
+dbPUP %>% # from dbPUP_clean.tsv
+  mutate(A = gsub("'","",A) ) %>% # clean a bit
+  merge(bif_pup, by.x="A", by.y = "X2") %>% # merge with bif diamond hit
+  merge(bif_length, by="X1") %>% #merge with gene length
+  mutate(ll=(X4*3)/X2) %>% filter(X3>50, ll>0.5) %>% # select id=50% over 50% protein alignment
+  group_by(X1,J) %>% top_n(n = 1, wt = X12) %>% # select top 1 by hit and by pup family
+
+
+
+
+
+
+
+
+
+
