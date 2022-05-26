7dbfa82cffd263e6565a2af0716b70a8158cba2a
Julien Tap <Julien.Tap@danone.com>
2022-05-26 03:06:13 +0200

    export dbPUP hits

--- data-raw/dbPUP_import.r
+++ data-raw/dbPUP_import.r
@@ -10,3 +10,17 @@
 #   readr::write_tsv(dbPUP_clean, file="dbPUP_clean.tsv")
 
 
+dbPUP %>% # from dbPUP_clean.tsv
+  mutate(A = gsub("'","",A) ) %>% # clean a bit
+  merge(bif_pup, by.x="A", by.y = "X2") %>% # merge with bif diamond hit
+  merge(bif_length, by="X1") %>% #merge with gene length
+  mutate(ll=(X4*3)/X2) %>% filter(X3>50, ll>0.5) %>% # select id=50% over 50% protein alignment
+  group_by(X1,J) %>% top_n(n = 1, wt = X12) %>% # select top 1 by hit and by pup family
+  dplyr::rename(id_rep=X1, pup_mag_gene=A, pup_family=J, tax=E, annot=B) %>%
+  select(id_rep, pup_mag_gene, pup_family, tax, annot) %>%
+  readr::write_tsv(file="data-raw/all.bif.nr95.dbPUP.hits.tsv")
+
+
+
+
+
